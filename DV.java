a11399dc3209fb812d5811f1a17d8e8f8e6ac5cd
dmsapereira <dmsa.pereira@campus.fct.unl.pt>
2019-12-12 20:40:44 +0000

    Ready for Delivery

--- DV.java
+++ DV.java
@@ -51,10 +51,12 @@ public class DV implements RoutingAlgorithm {
 
 	public void initalise() {
 		this.routingTable.put(
-			this.thisRouter.getId(), new DVRoutingTableEntry(this.thisRouter.getId(), 
-															LOCAL, 
-															0, 
-															this.thisRouter.getCurrentTime()));
+			this.thisRouter.getId(), 
+			new DVRoutingTableEntry(
+				this.thisRouter.getId(), 
+				LOCAL, 
+				0, 
+				this.thisRouter.getCurrentTime()));
 	}
 
 	public int getNextHop(int destination) {
@@ -67,53 +69,58 @@ public class DV implements RoutingAlgorithm {
 		return next.getInterface();
 	}
 
-	private void linkDown(int iface){
+	//Checks, for every entry, if the link is up. If not, set the metric to INFINITY
+	private void checkLinks(){
 		for(DVRoutingTableEntry entry : this.routingTable.values()){
-			if(entry.getInterface() == iface && entry.getMetric() != INFINITY){
+			if(!this.thisRouter.getInterfaceState(entry.getInterface()) && 
+			entry.getMetric() != INFINITY){
 				entry.setMetric(INFINITY);
 				entry.setTime(this.thisRouter.getCurrentTime());
 			}
 		}
 	}
 
-	public void tidyTable() {
-	for(Link zelda : this.thisRouter.getLinks()){
-			int thisSide = zelda.getRouter(0) == this.thisRouter.getId() ? 0 : 1;
-
-			if(!zelda.isUp())
-				this.linkDown(zelda.getInterface(thisSide));
-	}
-
-	if(this.expire){
+	//Checks all entries for expired ones. The ones that are found are deleted
+	private void cleanExpired(){
 		List<Integer> keys = new LinkedList<>();
 
-		for(int key : this.routingTable.keySet()){
-			keys.add(key);
-		}
+		//Not very efficient, but cant delete entries while iterating with a for each....
+		keys.addAll(this.routingTable.keySet());
 
 		for(int key : keys){
 			DVRoutingTableEntry entry = this.routingTable.get(key);
-			if(entry.getMetric() == INFINITY &&
-				this.thisRouter.getCurrentTime() - entry.getTime() > TIMEOUT * this.updateInterval)
+			int timeElapsed = this.thisRouter.getCurrentTime() - entry.getTime();
+			if(entry.getMetric() == INFINITY && timeElapsed > TIMEOUT * this.updateInterval)
 				this.routingTable.remove(key);
 		}
 	}
-}
+
+	public void tidyTable() {
+		this.checkLinks();
+
+		if(this.expire)
+			this.cleanExpired();
+	}
 
 	private Payload buildTableInformation(int iface){
 		String entryFormat = "%s %s";
 		Payload load = new Payload();
+
+		//Not very easy on the eyes, but havin the fors nested in the ifs removes the overhead
+		//of checking for pReverse on everytime in the ternary operator
 		if(this.pReverse){
 			for(DVRoutingTableEntry entry : this.routingTable.values()){
-					load.addEntry(String.format(entryFormat, 
-												entry.getDestination(),
-												entry.getInterface() == iface ? INFINITY : entry.getMetric()));
+					load.addEntry(String.format(
+						entryFormat, 
+						entry.getDestination(),
+						entry.getInterface() == iface ? INFINITY : entry.getMetric()));
 			}
 		}else{
 			for(DVRoutingTableEntry entry : this.routingTable.values()){
-				load.addEntry(String.format(entryFormat, 
-											entry.getDestination(),
-											entry.getMetric()));
+				load.addEntry(String.format(
+					entryFormat, 
+					entry.getDestination(),
+					entry.getMetric()));
 			}
 		}
 
@@ -123,46 +130,50 @@ public class DV implements RoutingAlgorithm {
 	public Packet generateRoutingPacket(int iface) {
 		Packet packet = new RoutingPacket(this.thisRouter.getId(), BROADCAST);
 		packet.setPayload(buildTableInformation(iface));
+
 		return packet; 
 	}
 
-	public void processRoutingPacket(Packet p, int iface) {
-		DVRoutingTableEntry entry;
+	//Given data about an entry received via a RoutingPacket, executes the appropiate logic 
+	private void processEntryData(int destination, int metric, int cost, int iface){
+		DVRoutingTableEntry entry = this.routingTable.get(destination);
 
+		//Nested because otherwise the else would be triggered even if the
+		//entry was null, leading to NullPointers
+		if(entry == null){
+			if(metric != INFINITY){
+				this.routingTable.put(destination, 
+					new DVRoutingTableEntry(destination, 
+											iface, 
+											metric + cost,
+											this.thisRouter.getCurrentTime()));
+			}
+		}else if(iface == entry.getInterface()){
+			int metricPre = entry.getMetric();
+			entry.setMetric(metric + cost > INFINITY ? INFINITY :  metric + cost);
+
+			//basically, if it was already INFINITY, then don't mess with the time
+			//only used for INFINITY, but we don't need the extra logic of checking for it
+			if(metricPre != metric)
+				entry.setTime(this.thisRouter.getCurrentTime());
+		}else if(metric + cost < entry.getMetric()){
+			entry.setInterface(iface);
+			entry.setMetric(metric + cost);
+			entry.setTime(this.thisRouter.getCurrentTime());
+		}
+	}
+
+	public void processRoutingPacket(Packet p, int iface) {
 		Iterator<Object> iterator = p.getPayload().getData().iterator();
 
-		if(!this.routingTable.containsKey(p.getSource())){
-			this.routingTable.put(p.getSource(), 
-				new DVRoutingTableEntry(p.getSource(), 
-										iface, 
-										this.thisRouter.getLinks()[iface].getInterfaceWeight(this.thisRouter.getId()), 
-										this.thisRouter.getCurrentTime()));
-		}
+		int cost = this.thisRouter.getLinks()[iface].getInterfaceWeight(this.thisRouter.getId());
 
 		while(iterator.hasNext()){
-			String data = (String)iterator.next();
+			String data = String.valueOf(iterator.next());
 			int destination = Integer.parseInt(data.split(" ")[0]);
 			int metric = Integer.parseInt(data.split(" ")[1]);
-			int cost = this.thisRouter.getLinks()[iface].getInterfaceWeight(this.thisRouter.getId());
 
-			if((entry = this.routingTable.get(destination)) == null){
-				if(metric != INFINITY){
-					this.routingTable.put(destination, 
-						new DVRoutingTableEntry(destination, 
-												iface, 
-												metric + cost,
-												this.thisRouter.getCurrentTime()));
-				}
-			}else if(iface == entry.getInterface()){
-				int metricPre = entry.getMetric();
-				entry.setMetric(metric + cost > INFINITY ? INFINITY :  metric + cost);
-				if(metricPre != metric)
-					entry.setTime(this.thisRouter.getCurrentTime());
-			}else if(metric + cost < entry.getMetric()){
-				entry.setInterface(iface);
-				entry.setMetric(metric + cost);
-				entry.setTime(this.thisRouter.getCurrentTime());
-			}
+			this.processEntryData(destination, metric, cost, iface);		
 		}
 	}
 
@@ -175,7 +186,7 @@ public class DV implements RoutingAlgorithm {
 
 /*
  * My Routing Table Entry
- */
+*/
 class DVRoutingTableEntry implements RoutingTableEntry {
 	int destination, iface, metric, time;
 
@@ -218,6 +229,7 @@ class DVRoutingTableEntry implements RoutingTableEntry {
 		this.time = t;
 	}
 
+	@Override
 	public String toString() {
 		String table_format = "d %d i %d m %d";
 		return String.format(table_format, this.destination, this.iface, this.metric);
